6e733b614bb4fb8a09cffd1e6fb156afb69c5950
rubennadaaal <ruben.nadal@insbaixcamp.cat>
2024-02-22 18:58:19 +0100

    canvi ruben

--- app/lab/xss/basic-reflected/index.php
+++ app/lab/xss/basic-reflected/index.php
@@ -14,44 +14,37 @@ $strings = tr();
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-  <title><?php echo $strings['title'];  ?></title>
+  <title><?php echo htmlspecialchars($strings['title'], ENT_QUOTES, 'UTF-8'); ?></title>
 </head>
 
 <body>
   <div class="container d-flex justify-content-center align-items-center h-100 mx-auto">
     <?php
-
+    // Comprobar si se proporciona un parámetro 'q' en la URL
     if (isset($_GET['q'])) {
-      $q = $_GET['q'];
+      $q = htmlspecialchars($_GET['q'], ENT_QUOTES, 'UTF-8');
+      // Mostrar mensaje de alerta de error
       echo '<div class="alert alert-danger" style="margin-top: 30vh;" role="alert" >';
-      echo '' . $strings['text'] . ' <b>' . $q . '</b> ';
-      echo '<a href="index.php" ">' . $strings['try'] . '</a>';
+      echo '' . htmlspecialchars($strings['text'], ENT_QUOTES, 'UTF-8') . ' <b>' . $q . '</b> ';
+      echo '<a href="index.php">' . htmlspecialchars($strings['try'], ENT_QUOTES, 'UTF-8') . '</a>';
       echo "</div>";
     } else {
+      // Mostrar formulario de búsqueda si no se proporciona 'q'
       echo '<form method="GET" action="#" style="margin-top: 30vh;" class="row g-3 col-md-6 row justify-content-center mx-auto">';
-      echo '<input class="form-control" type="text" placeholder="' . $strings['search'] . '" name="q">';
-      echo '<button type="submit" class="col-md-3 btn btn-primary mb-3">' . $strings['s_button'] . '</button>';
+      echo '<input class="form-control" type="text" placeholder="' . htmlspecialchars($strings['search'], ENT_QUOTES, 'UTF-8') . '" name="q">';
+      echo '<button type="submit" class="col-md-3 btn btn-primary mb-3">' . htmlspecialchars($strings['s_button'], ENT_QUOTES, 'UTF-8') . '</button>';
       echo '</form>';
     }
 
-    // Validar el comentario
-    $comentario = trim($_POST["comentario"]);
-    if(empty($comentario)){
-      exit("Debes proporcionar un comentario");
-    }
-    // Sanitizar comentario
-    $comentario = strip_tags($comentario);
-    // El comentario ya se puede guardar de forma segura
-    file_put_contents("comentarios.txt", $comentario, FILE_APPEND);
-    // Escapar comentarios antes de mostrarlos
-    $comentarios = file_get_contents("comentarios.txt");
-    echo htmlspecialchars($comentarios);
     ?>
 
   </div>
+    
 
-  <script id="VLBar" title="<?= $strings['title'] ?>" category-id="1" src="/public/assets/js/vlnav.min.js"></script>
+  <!-- Cerrar etiqueta </html> -->
+  </html>
 
+  <!-- Sección opcional para cargar scripts JavaScript adicionales según sea necesario -->
 </body>
 
 </html>
