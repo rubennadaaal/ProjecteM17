bd96eef275a73c7bdc4c35e69a0165e5d8dceee0
joanporta23 <joan.porta@insbaixcamp.cat>
2024-02-22 19:36:56 +0100

    brute

--- app/lab/broken-authentication/brute-force/brute.php
+++ app/lab/broken-authentication/brute-force/brute.php
@@ -1,26 +1,38 @@
 <?php
 
+session_start(); 
+
+if (!isset($_SESSION['login_attempts'])) {
+    $_SESSION['login_attempts'] = 0; 
+}
+
+$max_attempts = 3; 
+
 $db = new PDO('sqlite:users.db');
-$html= "";
+$html = "";
 
-if(isset($_POST['username']) && isset($_POST['password'])  ){
-    $q = $db->prepare("SELECT * FROM users_ WHERE username=:user AND password=:pass");
-    $q->execute(array(
-        'user' => $_POST['username'],
-        'pass' => $_POST['password']
-    ));
-    $_select = $q -> fetch();
+if (isset($_POST['username']) && isset($_POST['password'])) {
+    $_SESSION['login_attempts']++;
 
-    if ( isset($_select['id'])) {
-
-        session_start();
-        $_SESSION['username'] = $_POST['username'];
-        $html =$strings["cong"];
-        //header("Location: index.php");
+    if ($_SESSION['login_attempts'] > $max_attempts) {
+        $html = "Masses intents.";
     } else {
-        $html=$strings["wrong"];
-    }
+        $q = $db->prepare("SELECT * FROM users_ WHERE username=:user AND password=:pass");
+        $q->execute(array(
+            'user' => $_POST['username'],
+            'pass' => $_POST['password']
+        ));
+        $_select = $q->fetch();
 
+        if (isset($_select['id'])) {
+            $_SESSION['username'] = $_POST['username'];
+            $html = "¡Inicio de sesión exitoso!";
+            // Redireccionar a la página de inicio
+            // header("Location: index.php");
+        } else {
+            $html = "Worng.";
+        }
+    }
 }
 
 ?>
